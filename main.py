eaf31d7103dab300509a4a3da81776de895f504c
LEBsci <github@botello.me>
2024-12-08 22:06:37 +0100

    decission tree classifier model

--- main.py
+++ main.py
@@ -1,52 +1,108 @@
-# Import necessary libraries
+'''Import necessary libraries'''
 import pandas as pd
-import mlflow, argparse
+import numpy as np
+import mlflow, argparse, requests
 
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import ConfusionMatrixDisplay
-
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.tree import DecisionTreeClassifier
 
+
+'''Functions to clean and organize the data'''
+
+def question_to_nan(df):
+    df = df.replace('?', np.nan)
+    return df
+
+# Function to drop non numerical columns except 'income'
+def drop_non_numerical(df):
+    non_numerical_columns = df.select_dtypes(exclude=[np.number]).columns
+    non_numerical_columns = non_numerical_columns.drop('income')
+    df = df.drop(columns=non_numerical_columns)
+    return df
+
+# Function to change the 'income' column to categorical
+def encode_income(df):
+    df['income'] = pd.Categorical(df['income'])
+    print("Categories and their encoded values:")
+    print(dict(enumerate(df['income'].cat.categories)))
+    df['income'] = df['income'].cat.codes
+    return df
+
+'''Functions to transform the data'''
+# Define an imputer for the median of the train data and apply to both train and test
+imputer = SimpleImputer(strategy='median')
+def impute_data(df):
+    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+    return df
+
+'''Define the pipelines'''
+# Cleaning and preprocessing
+data_preprocessing = Pipeline([
+    ('question_to_nan', FunctionTransformer(question_to_nan)),
+    ('drop_non_numerical', FunctionTransformer(drop_non_numerical)),
+    ('encode_income', FunctionTransformer(encode_income))
+])
+
+# Transformations and modifications
+data_transformations = Pipeline([
+    ('impute_data', FunctionTransformer(impute_data))
+])
+
+'''Define the metrics and the model'''
+
+# Function to determine the metrics of the model
 def eval_metrics(actual, pred):
     mse = mean_squared_error(actual, pred)
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
     return mse, mae, r2
 
-def train_model(max_iter=200):
+def train_model():
 
-    # Load the Iris dataset
-    iris = load_iris()
+    # Download the dataset
+    url = "https://raw.githubusercontent.com/pooja2512/Adult-Census-Income/master/adult.csv"
+    response = requests.get(url)
+    with open("adult.csv", "wb") as f:
+        f.write(response.content)
+
+    # Load the dataset
+    data = pd.read_csv("adult.csv")
     
-    # Transform the dataset into a DataFrame
-    df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-    df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+    # Preprocess the data
+    data_prepared = data_preprocessing.fit_transform(data)
     
-    # Split the dataset into training and testing sets
-    x, xt, y, yt = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
-    # Start to log an experiment
+    # Divide the data into test and train with stratify on the target
+    train_data, test_data = train_test_split(data_prepared, test_size=0.2, stratify=data_prepared['income'], random_state=42)
 
-    # print(f"Starting the experiment with max_iter={max_iter}")
+    # Define the features and the target
+    X_train = train_data.drop(columns='income')
+    y_train = train_data['income']
+    
+    X_test = test_data.drop(columns='income')
+    y_test = test_data['income']
 
-    # Log the parameters
-    mlflow.log_param("max_iter", max_iter)
+    # Transform the data
+    X_train = data_transformations.fit_transform(X_train)
+    X_test = data_transformations.transform(X_test)
 
-    # Create a Logistic Regression model
-    model = LogisticRegression(max_iter=max_iter)
+    # Create a Decision Tree model
+    model = DecisionTreeClassifier()
 
     # Fit the model
-    model.fit(x, y)
+    model.fit(X_train, y_train)
 
     # Log the model
-    mlflow.sklearn.log_model(model, "model")
+    mlflow.sklearn.log_model(model, "DecisionTreeModel")
 
     # Predict the test set
-    y_pred = model.predict(xt)
+    y_pred = model.predict(X_test)
 
-    # Check the metrics
-    mse, mae, r2 = eval_metrics(yt, y_pred)
+    # Evaluate the model
+    mse, mae, r2 = eval_metrics(y_test, y_pred)
 
     # Log the metrics
     mlflow.log_metric("mse", mse)
@@ -54,7 +110,4 @@ def train_model(max_iter=200):
     mlflow.log_metric("r2", r2)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--max_iter", type=int, default=200)
-    args = parser.parse_args()
-    train_model(args.max_iter)
+    train_model()
